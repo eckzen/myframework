6f82bd5a502d99f415c705590ce90cdea5e55eb0
eckzen <eckzen@yahoo.com>
2015-07-29 15:40:34 +0800

    starting point

--- index.php
+++ index.php
@@ -10,23 +10,13 @@
 
 </head>
 <body>
-	<div class="container-fluid">
-	<section class="row">
-	<div class="col col-lg-8">
-	<h1>Hello Renren?!!!</h1>
-	</div>
-	<div class="col col-lg-4">
-	<h1>Happy Birthday anak!!!!</h1>
-	</div>
-
-	</section>
-	</div>
+	
 
 
 
 
-	<script src="_/js/bootstrap.js"></script>
 	<script src="_/js/jquery.js"></script>
+	<script src="_/js/bootstrap.js"></script>
 	<script src="_/js/myscript.js"></script>
 	<script type='text/javascript' id="__bs_script__">//<![CDATA[
 	    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.8.0.js'><\/script>".replace("HOST", location.hostname));
